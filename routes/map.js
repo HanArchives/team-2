320f735a4eb25eef36bed8a3cd1108c3d443964f
JonnaVisser <94176438+JonnaVisser@users.noreply.github.com>
2022-06-14 12:22:01 +0200

    add:feature/filter/map

--- routes/map.js
+++
@@ -1,8 +0,0 @@
-// const express = require('express');
-// const router = express.Router();
-// const map = require('../controller/MapController');
-
-// router.post('/', map.map);
-// router.get('/', map.map);
-
-// module.exports = router;
