5b6aa15e4a420a46dcfac5d8f34c926d4c72da6c
thijew <thijewind@hotmail.com>
2022-06-20 16:51:08 +0200

    ADD feedback when updating your profile

---
+++ static/scripts/update-profile.js
@@ -0,0 +1,6 @@
+const updateBtn = document.querySelector('.updateBtn');
+const updateText = document.querySelector('.updateProfile');
+
+const showText = updateBtn.addEventListener('click', () => {
+  updateText.style.display = 'block';
+});
